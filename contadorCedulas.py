670c819723bd30c3c1910b2502ce08d38e393c73
DiogoEngh <97733618+DiogoEngh@users.noreply.github.com>
2022-05-10 10:29:07 -0300

    Update contadorCedulas.py

--- contadorCedulas.py
+++ contadorCedulas.py
@@ -1,8 +1,7 @@
 ##Programa que dado um valor ele retornara o numero minimo de cedulas a sacar
 valores = [200,100,50,20,10,5,2]
 valor = float(input("Digite o valor a sacar: \n"))
-i = 0
-contador = 0
+i = 0, contador = 0
 while valor>=2:
   if valor >= valores[i]:
     valor-=valores[i]
